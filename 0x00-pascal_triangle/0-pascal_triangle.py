dbcc073d6414b4c1ec2ad48d60c181eda460a9a4
Bahy Allam <66816838+BahyAllaAllam@users.noreply.github.com>
2024-06-30 04:42:27 +0300

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -10,16 +10,14 @@ def pascal_triangle(n):
     representing the Pascal Triangle of n
     returns empty list if n <= 0
     """
+    triangle = []
     if n <= 0:
-        return []
-    
+        return triangle
     triangle = [[1]]
-    
     for i in range(1, n):
         row = [1]
         for j in range(1, i):
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
         row.append(1)
         triangle.append(row)
-    
     return triangle
