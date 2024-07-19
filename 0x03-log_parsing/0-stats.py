706f776da668550f610a0a8d98afd396c0d34fbc
BahyAllaAllam <bahy.alam@yahoo.com>
2024-07-19 05:17:29 -0700

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,67 +1,43 @@
 #!/usr/bin/python3
 """
-Log parsing script
+log parsing problem
 """
-
-
-import sys
 import re
-import signal
+from sys import stdin
 
 
 if __name__ == '__main__':
-    line_count = 0
-    log = {
-            "file_size": 0,
-            "code_frequency": {
-                "200": 0,
-                "301": 0,
-                "400": 0,
-                "401": 0,
-                "403": 0,
-                "404": 0,
-                "405": 0,
-                "500": 0
-                }
-            }
+    state_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                   403: 0, 404: 0, 405: 0, 500: 0}
 
-    def print_stats(log):
-        """Print the statistics"""
-        print("File size: {}".format(log["file_size"]))
-        for code in sorted(log["code_frequency"]):
-            if log["code_frequency"][code] > 0:
-                print("{}: {}".format(code, log["code_frequency"][code]))
+    def print_codes(codes, total_size):
+        """
+        Print file sizes and status codes.
+        """
+        print(f'File size: {total_size}')
+        for k, v in sorted(codes.items()):
+            if v != 0:
+                print(f'{k}: {v}')
 
-    def signal_handler(sig, frame):
-        """Handle keyboard interruption"""
-        print_stats(log)
-        sys.exit(0)
+    count, filesize = 0, 0
+    lines = []
 
-    signal.signal(signal.SIGINT, signal_handler)
-    log_pattern = re.compile(
-            r"""
-            (\d{1,3}\.){3}\d{1,3}
-            \s-\s
-            \[\d{4}-\d{2}-\d{2}
-            \s\d{2}:\d{2}:\d{2}\.\d+\]
-            \s"GET\s/projects/260\sHTTP/1\.1"
-            \s(\d{3})
-            \s(\d+)
-            """, re.VERBOSE
-            )
     try:
-        for line in sys.stdin:
-            line = line.strip()
-            match = log_pattern.match(line)
-            if match:
-                line_count += 1
-                status_code = match.group(2)
-                file_size = int(match.group(3))
-                log["file_size"] += file_size
-                if status_code in log["code_frequency"]:
-                    log["code_frequency"][status_code] += 1
-                if line_count % 10 == 0:
-                    print_stats(log)
-        print_stats(log)
-    except Exception:
-        pass
+        for line in stdin:
+            count += 1
+            splitted = line.split(' ')
+            try:
+                code = int(splitted[-2])
+                if code in state_codes:
+                    state_codes[code] += 1
+            except BaseException:
+                pass
+            try:
+                filesize += int(splitted[-1])
+            except BaseException:
+                pass
+            if count % 10 == 0:
+                print_codes(state_codes, filesize)
+        print_codes(state_codes, filesize)
+    except BaseException:
+        print_codes(state_codes, filesize)
